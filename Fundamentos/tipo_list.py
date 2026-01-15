54d6175075487a4ecb42181e9fbfeaafe9e150d2
Rafael Gonzaga <rafaelgonzagacontato@gmail.com>
2026-01-15 08:33:23 -0300

    Adiciona aula de tipo list - Introdução à listas em Python

---
+++ Fundamentos/tipo_list.py
@@ -0,0 +1,14 @@
+## Introdução às listas mutáveis no Python.
+
+"""
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizaveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+
+"""
+
+string = 'ABCDE' # 5 CARACTERES/INDICE 0 A 4 - IMUTÁVEL
+lista = [123, True, 'Rafael Gonzaga', 1.2, []]
+lista[-3] = 'Maria'
+print(lista)
