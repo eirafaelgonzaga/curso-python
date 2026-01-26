027711199a56e8dc045f33f1910bda50a143c040
Rafael Gonzaga <rafaelgonzagacontato@gmail.com>
2026-01-26 06:37:28 -0300

    Adiciona aula de métodos em python. pop, append, del

---
+++ Fundamentos/metodos_del_pop.py
@@ -0,0 +1,14 @@
+lista = [10, 20, 30, 40, 50, 60]
+print(lista)
+del lista[2]
+print(lista)
+lista[2] = 'Oi'
+print(lista)
+lista.pop()
+print(lista)
+lista[2] = 30
+print(lista)
+lista.pop()
+print(lista)
+lista.append(40)
+print(lista)
